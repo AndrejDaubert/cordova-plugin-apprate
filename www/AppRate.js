06f6777fd7301fa260488f8a209347e64784f54c
Andrej Daubert <>
2021-01-22 12:29:51 +0100

    Fix iPad detection

--- www/AppRate.js
+++ www/AppRate.js
@@ -40,7 +40,7 @@ var AppRate = (function() {
 
   var LOCAL_STORAGE_COUNTER = 'counter';
 
-  var IS_IOS = /(iPhone|iPod|iPad)/i.test(navigator.userAgent.toLowerCase());
+  var IS_IOS = /(iPhone|iPod|iPad)/i.test(navigator.userAgent.toLowerCase()) || ((navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 0) || navigator.platform === 'iPad');
   var IS_ANDROID = /Android/i.test(navigator.userAgent.toLowerCase());
   var FLAG_NATIVE_CODE_SUPPORTED = IS_IOS || IS_ANDROID;
 
